cacd536bf15087dddb5b2fe38b8a8a74732e21cf
Wontae <bbrick@Wontae-3.local>
2022-08-06 11:54:55 +0900

    Feat: new branches

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,1 +1,2 @@
 export * from "./useGetTokens";
+export * from "./useGetNFTMeta";
---
+++ src/hooks/useGetNFTMeta.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import { Connection, clusterApiUrl, PublicKey } from "@solana/web3.js";
+
+function useGetNFTMeta(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export {useGetNFTMeta};
