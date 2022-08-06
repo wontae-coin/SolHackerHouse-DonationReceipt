1650674199c38d8a37df51d796316b49a19bf574
paulyoo <paul@bbrick.com>
2022-08-07 01:41:34 +0900

    modified get uris for loop to async (promise)

--- src/hooks/useGetNftUris.js
+++ src/hooks/useGetNftUris.js
@@ -8,28 +8,46 @@ import axios from 'axios';
 
 function useGetNftUris(walletPubkey) {
     const [nftUris, setNftUris] = useState([]);
+    const connection = new Connection(clusterApiUrl("mainnet-beta"), 'confirmed');
 
-    const walletToQuery = new PublicKey(walletPubkey);
     async function getNftUris() {
-        const connection = new Connection(clusterApiUrl("mainnet-beta"), 'confirmed');
-        const response = await connection.getTokenAccountsByOwner(walletToQuery, { programId: SPLToken.TOKEN_PROGRAM_ID});
+        let walletToQuery;
+        let tokenAccounts;
+        try{
+            walletToQuery = new PublicKey(walletPubkey);
+            tokenAccounts = await connection.getTokenAccountsByOwner(walletToQuery, { programId: SPLToken.TOKEN_PROGRAM_ID});
+        }catch (err){
+            console.log(err);
+            return;
+        }
 
         let l_nftUris = [];
-        for (const token of response.value) {
+        console.log('tokenAccounts.value.length', tokenAccounts.value.length);
+        console.log('tokenAccounts', tokenAccounts);
+
+        async function getTokensUris(token){
             try{
                 let accountInfo = SPLToken.AccountLayout.decode(token.account.data);
                 let tokenmetaPubkey = await Metadata.getPDA(accountInfo.mint);
                 const tokenmeta = await Metadata.load(connection, tokenmetaPubkey);
                 console.log(tokenmeta.data.data.uri);
                 let res = await axios.get(tokenmeta.data.data.uri);
+
                 console.log(res.data.properties.files[0].uri);
                 if (res.data.properties.files[0].uri){
                     l_nftUris.push(res.data.properties.files[0].uri);
                 }
             }catch (err){
                 console.log(err);
-
             }
+        }
+        // map array, tokenAccounts.value, to promises
+        const promises = tokenAccounts.value.map(getTokensUris);
+        // wait until all promises are resolved
+        await Promise.all(promises);
+        
+        if (l_nftUris.length == 0){
+            l_nftUris.push("https://drive.google.com/uc?export=view&id=18wYCEWnuduEaEC7Bmm6_EZqXqrjbU6fs");
         }
         setNftUris(l_nftUris);
     }
